611c9c5e9b200b124fe1aa2c70c8e4d5f1f149d6
Kristian Hasselknippe <kristian@nortero.com>
2024-01-12 09:01:45 +0100

    added parameter to keywords list

--- midio-mode.el
+++ midio-mode.el
@@ -2,7 +2,7 @@
 
 ;; Define keywords, built-in types, and attribute names
 (defvar midio-font-lock-keywords
-  `(("\\<\\(native_dependencies_location\\|extern\\|true\\|false\\|module\\|type\\|func\\|event\\|in\\|out\\|trigger\\|property\\)\\>" . font-lock-keyword-face)
+  `(("\\<\\(native_dependencies_location\\|extern\\|parameter\\|true\\|false\\|module\\|type\\|func\\|event\\|in\\|out\\|trigger\\|property\\)\\>" . font-lock-keyword-face)
     ("\\<\\(Number\\|String\\|Bool\\|Any\\|Opaque\\)\\>" . font-lock-type-face)
     ("\\<\\(doc\\|hidden\\|consumes\\|borrows\\|provides\\|x\\|y\\|name\\)\\>" . font-lock-builtin-face)
     ("\"[^\"]*\"" . font-lock-string-face)
